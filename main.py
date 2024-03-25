c3cd3fe8c47277c83c029950da966a3323a11386
Juan Campuzano <juanfecazuluaga@gmail.com>
2024-03-24 21:39:02 -0500

    Espero 5 segundos antes de iniciar

--- main.py
+++ main.py
@@ -343,12 +343,11 @@ def extraer_hard_skills(texto):
   return list(set(lista_hard_skills))
 
 def descargar_data_cv(id_cv):
+  time.sleep(5)
   client = MongoClient("mongodb+srv://danielCTO:Coally2023-123@coally.nqokc.mongodb.net/CoallyProd?authSource=admin&replicaSet=atlas-39r1if-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true")
   db = client['CoallyProd']
   db_cvs = db['usercvs']
   data_cv = db_cvs.find_one({'_id':ObjectId(id_cv)})
-
-  print('---'+id_cv+'---')
 
   if not data_cv:
     client = MongoClient("mongodb+srv://danielCTO:Coally2023-123@coally.nqokc.mongodb.net/CoallyProd?authSource=admin&replicaSet=atlas-39r1if-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true")
