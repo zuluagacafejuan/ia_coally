07620eab59d430d4eb2ce83c919418f64a70522e
Juan Felipe <juanfecazuluaga@gmail.com>
2024-06-05 21:32:32 -0500

    Fix

--- main.py
+++ main.py
@@ -941,7 +941,7 @@ def agregar_proyecto(id_proyecto, uniandes=False):
   toc = time()
 
   print('extraer_features', toc-tic)
-  cluster, vector = clusterizar((data_proyecto_transformada['NombreOportunidad']+' '+data_proyecto_transformada['DescribeProyecto']).replace('~',','))
+  cluster, vector = clusterizar((data_proyecto_transformada['DescribeProyecto']).replace('~',','))
 
 
   cluster = 0
